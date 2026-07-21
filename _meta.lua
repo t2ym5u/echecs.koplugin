deb91aed018f734aae131ae1a53ab1bce0f907a6
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 16:25:48 +0200

    chore: bump version to 1.1.6

--- _meta.lua
+++ _meta.lua
@@ -1,7 +1,7 @@
 local _ = require("gettext")
 return {
     name        = "echecs",
-    version     = "1.1.5",
+    version     = "1.1.6",
     fullname    = _("Échecs"),
     description = _("Jeu d'échecs avec IA"),
 }
